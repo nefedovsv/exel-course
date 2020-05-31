9265c389404c33595a0ead030871769b980d0cd6
nefedovsv <serg.nefedoff@yandex.ru>
2020-05-31 21:03:11 +0300

    resize 31.05.2020

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,37 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
+import {$} from '@core/dome';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    });
+  }
+
   toHTML() {
     return createTable()
+  }
+
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      const $resizer = $(event.target)
+      const $parent = $resizer.closest('[data-type ="resizable"]')
+      const coords = $parent.getCoords()
+
+
+      document.onmousemove = e => {
+        const delta = e.pageX - coords.right
+        const value = coords.width + delta
+        $parent.$el.style.width = value + 'px'
+        document.querySelectorAll(`[data-col="${$parent.data.col}"]`).forEach(el => el.style.width = value + 'px')
+      }
+
+      document.onmouseup = e => {
+        document.onmousemove = null
+      }
+    }
   }
 }
