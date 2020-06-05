e6e4bcc5a05ae4ad7bdf8023b2f3f5721e2091eb
nefedovsv <serg.nefedoff@yandex.ru>
2020-06-05 21:36:45 +0300

    finish base table logic

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,24 +1,44 @@
 import {ExcelComponent} from '@core/ExcelComponent';
+import {$} from '@core/dome';
 
 export class Formula extends ExcelComponent {
   static className = 'excel__formula'
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Formula',
-      listeners: ['input']
+      listeners: ['input', 'keydown'],
+      ...options
     });
   }
 
   toHTML() {
     return `
             <div class="info">fx</div>
-            <div class="input" contenteditable spellcheck="false">  
+            <div id='formula' class="input" contenteditable spellcheck="false">  
 `;
   }
 
+  init() {
+    super.init();
+    this.$formula = this.$root.find('#formula')
+    this.$on('table:select', $cell => {
+      this.$formula.text($cell.text())
+    })
+    this.$on('table:input', $cell => {
+      this.$formula.text($cell.text())
+    })
+  }
+
   onInput(event) {
-    console.log(this.$root)
-    console.log(event.target.textContent)
+    this.$emit('formula:input', $(event.target).text())
+  }
+
+  onKeydown(event) {
+    const keys = ['Enter', 'Tab']
+    if (keys.includes(event.key)) {
+      event.preventDefault()
+      this.$emit('formula-done')
+    }
   }
 }
